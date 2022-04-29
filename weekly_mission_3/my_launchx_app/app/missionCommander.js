c8731e80064790be7b11d8d8b0e5c6ccd4b63daa
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-04-28 20:41:49 -0500

    Creación de una clase y modularización

---
+++ weekly_mission_3/my_launchx_app/app/missionCommander.js
@@ -0,0 +1,12 @@
+
+// Creating the Mission Commander class
+class MissionCommander {
+    constructor (name) {
+        this.name = name
+    }
+
+    
+}
+
+// With this line we can export the class
+module.exports = MissionCommander
---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+
+const carlo = new MissionCommander("Carlo")
+const lalo = new MissionCommander("Lalo")
+const sandra = new MissionCommander("Sandra")
+
+console.log(carlo.name)
+console.log(lalo.name)
+console.log(sandra.name)
