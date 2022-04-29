be53594444ed155b960aac4a06a110801f0cc591
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-04-29 13:51:08 -0500

    Agregando la prueba de una nueva funcionalidad de la clase User

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -6,15 +6,17 @@ describe('Unit Test for User class', () => {
     test('Test 1.- Create an User object', () => {
 
         // Create an User object
-        const user = new User(1, 'Lalo', 'Eduardo Solis', 'Bio', 'dateCreated', 'lastUpdated')
+        const user = new User(1, 'Lalo', 'Eduardo Solis', 'Bio')
 
         // Validate the User object
         expect(user.id).toBe(1);
         expect(user.username).toBe('Lalo');
         expect(user.name).toBe('Eduardo Solis');
         expect(user.bio).toBe('Bio');
-        expect(user.dateCreated).toBe('dateCreated');
-        expect(user.lastUpdated).toBe('lastUpdated');
+
+        // This line check the value isn't "undefined"
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
 
     });
 
