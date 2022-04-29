63f29c97eb15775d117b45841219b87dfe8e75be
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-04-28 21:47:42 -0500

    Actualizando la prueba para la funcionalidad que se agregara

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,6 +1,14 @@
-describe('Test Suite Dummy Description', () => {
-    test('Case 1 Dummy', () => { 
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18);
+describe('Unit Test for Spiderman class', () => {
+    test('Test 1.- Create an spiderman object', () => { 
+        
+        // Here we writing the code we want to use
+        // I want to instantiate a Spiderman object with this information
+        const andrewGarfield = new Spiderman('Spiderman Sony', 31, 'Andrew Garfield', 2, 'Sony')
+
+        expect(andrewGarfield.name).toBe('Spiderman Sony');
+        expect(andrewGarfield.age).toBe(31);
+        expect(andrewGarfield.actor).toBe('Andrew Garfield');
+        expect(andrewGarfield.numberOfMovies).toBe(2);
+        expect(andrewGarfield.studio).toBe('Sony');
     });
 })
