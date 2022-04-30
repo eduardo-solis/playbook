ebd6881f0f7a7eb98b9cad102139bee3167f5e8d
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-04-29 21:30:13 -0500

    Creando el codigo para pasar la prueba de la nueva funcionalidad

--- weekly_mission_3/twitter/app/services/UserService.js
+++ weekly_mission_3/twitter/app/services/UserService.js
@@ -9,6 +9,21 @@ class UserService {
 
     }
 
+    static getInfo(user) {
+
+        let list = []
+        
+        list.push(user.id)
+        list.push(user.username)
+        list.push(user.name)
+        list.push(user.bio)
+        list.push(user.dateCreated)
+        list.push(user.lastUpdated)
+
+
+        return list
+    }
+
 }
 
 module.exports = UserService
