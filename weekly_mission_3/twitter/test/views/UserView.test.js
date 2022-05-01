1db486bc66403e523186028d91612102da139c1c
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-01 13:35:56 -0500

    Agregando la prueba para la clase UserView

---
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -0,0 +1,15 @@
+
+const UserView = require('./../../app/views/UserView')
+
+describe('Test for UserView', () => {
+
+    test('Return an error object when try to create a new user with an null payload', () => {
+
+        const payload = null
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/payload no existe/)
+
+    })
+
+})
