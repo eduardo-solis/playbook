2a4365d6e8c31263bb612d7e1593bcd213d051c4
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-01 14:04:54 -0500

    Prueba para el requeriminto 3 - UserView

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -21,4 +21,13 @@ describe('Test for UserView', () => {
 
     })
 
+    test('Return an error object when try to create a new user with a payload with missing properties', () => {
+
+        const payload = { username : 'Username' }
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+
+    })
+
 })
